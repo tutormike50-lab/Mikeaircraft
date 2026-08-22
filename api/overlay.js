bf154f29116029db68250879e7e7a1f23b64a9f0
tutormike50-lab <tutormike50@gmail.com>
2026-08-22 17:41:55 +0200

    Re-enable stabilized Overlay v1.5

--- api/overlay.js
+++ api/overlay.js
@@ -1,3 +1,3 @@
-// MikeAircraft Livestream Overlay v1.4
+// MikeAircraft Livestream Overlay v1.5
 // Thin API entrypoint; implementation lives under /lib.
-module.exports = require("../lib/overlay-wrapper-v14.js");
+module.exports = require("../lib/overlay-wrapper-v15.js");
