821e46c45b380993cd2abbcbab6f888a47e7b348
tutormike50-lab <tutormike50@gmail.com>
2026-08-22 18:04:22 +0200

    Switch production overlay to v1.6 without map

--- api/overlay.js
+++ api/overlay.js
@@ -1,3 +1,3 @@
-// MikeAircraft Livestream Overlay v1.5
+// MikeAircraft Livestream Overlay v1.6
 // Thin API entrypoint; implementation lives under /lib.
-module.exports = require("../lib/overlay-wrapper-v15.js");
+module.exports = require("../lib/overlay-wrapper-v16.js");
