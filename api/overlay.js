bd91663a9ce934e3a5cfd5115781b62d5727d469
tutormike50-lab <tutormike50@gmail.com>
2026-08-23 12:32:04 +0200

    Switch live overlay to v2.7 flashing moments

--- api/overlay.js
+++ api/overlay.js
@@ -1,3 +1,3 @@
-// MikeAircraft Livestream Overlay v2.6
+// MikeAircraft Livestream Overlay v2.7
 // Thin API entrypoint; implementation lives under /lib.
-module.exports = require("../lib/overlay-wrapper-v26.js");
+module.exports = require("../lib/overlay-wrapper-v27.js");
