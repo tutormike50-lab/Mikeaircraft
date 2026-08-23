1cfd65e690673af86c2db9987f1e4f4c78ec1f38
tutormike50-lab <tutormike50@gmail.com>
2026-08-23 12:19:44 +0200

    Switch live overlay to v2.5 story-only ticker

--- api/overlay.js
+++ api/overlay.js
@@ -1,3 +1,3 @@
-// MikeAircraft Livestream Overlay v2.4
+// MikeAircraft Livestream Overlay v2.5
 // Thin API entrypoint; implementation lives under /lib.
-module.exports = require("../lib/overlay-wrapper-v24.js");
+module.exports = require("../lib/overlay-wrapper-v25.js");
