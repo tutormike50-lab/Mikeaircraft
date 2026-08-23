d32b58d7d2b34e2febb57893d4f4dcbaebbd2afc
tutormike50-lab <tutormike50@gmail.com>
2026-08-23 12:36:52 +0200

    Switch live overlay to v2.8 moment styling

--- api/overlay.js
+++ api/overlay.js
@@ -1,3 +1,3 @@
-// MikeAircraft Livestream Overlay v2.7
+// MikeAircraft Livestream Overlay v2.8
 // Thin API entrypoint; implementation lives under /lib.
-module.exports = require("../lib/overlay-wrapper-v27.js");
+module.exports = require("../lib/overlay-wrapper-v28.js");
