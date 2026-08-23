48376d49bdddeea3f538784d4e59fa423fe6b2c8
tutormike50-lab <tutormike50@gmail.com>
2026-08-23 11:31:03 +0200

    Point overlay API to v1.8

--- api/overlay.js
+++ api/overlay.js
@@ -1,3 +1,3 @@
-// MikeAircraft Livestream Overlay v1.7
+// MikeAircraft Livestream Overlay v1.8
 // Thin API entrypoint; implementation lives under /lib.
-module.exports = require("../lib/overlay-wrapper-v17.js");
+module.exports = require("../lib/overlay-wrapper-v18.js");
