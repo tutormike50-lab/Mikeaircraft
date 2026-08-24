7e9f6d19d16f7099399bd1be0885dedf14ef1596
tutormike50-lab <tutormike50@gmail.com>
2026-08-24 09:15:10 +0200

    Connect primary overlay to laptop airport control

--- api/overlay.js
+++ api/overlay.js
@@ -1,3 +1,3 @@
-// MikeAircraft Livestream Overlay v2.8
-// Thin API entrypoint; implementation lives under /lib.
-module.exports = require("../lib/overlay-wrapper-v28.js");
+// MikeAircraft Livestream Overlay v2.9
+// Airport selection is controlled from /api/control.
+module.exports = require("../lib/overlay-wrapper-v29-control.js");
